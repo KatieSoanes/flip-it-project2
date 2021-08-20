d300429532ed7c0f6f7c0becf8419a9299f7bc09
KatieSoanes <katiesoanes@hotmail.com>
2021-08-20 17:51:08 +0000

    Add class to on click button for Game Rules styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,134 +1,537 @@
-//Welcome TO ALERT NOT TO BE USED
-//function myFunction(){
- // alert("Page is loaded");
-//}
-
- //Reset game button
- // Code inspired by Junior Developer Central - How to refresh/reload a page with JavaScript Youtube tutorial.
- document.querySelector('#reload')
- document.querySelector('#reload').addEventListener('click', () => {
-   window.location.reload(true);
- }); 
 
 
-// code on how to match cards taken from Marina Ferreira (freeCodeCamp.Org) Youtube video: "Memory Card Game-JavaScript Tutorial"
-// Match cards by data type, same card click (prevent card getting stuck if double clicked) and lock-board// 
-const cards = document.querySelectorAll('.flip-card');
+// new variables
 
-  let hasFlippedCard = false;
-  let lockBoard = false;
-  let firstCard, secondCard;
- 
+let noOfMatches = 0
 
-  function flipCard() {
-    if (lockBoard) return;
-   if (this === firstCard) return;
+let currentTime = 60
 
-    this.classList.add('flip');
+let gameOn = true
 
-    if (!hasFlippedCard) {
-      hasFlippedCard = true;
-      firstCard = this;
-      return;
-    }
 
-    secondCard = this;
 
-    checkForMatch();
-  }
 
-  function checkForMatch() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-    isMatch ? disableCards() : unflipCards();
-  }
+let modalDisplay
 
-  function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
+let backgroundDisplay
 
-   resetBoard();
-  }
 
-  function unflipCards() {
-    lockBoard = true;
 
-    setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
 
-     resetBoard();
-    }, 600);
-  }
+//Reset game button
 
- function resetBoard() {
-   [hasFlippedCard, lockBoard] = [false, false];
-   [firstCard, secondCard] = [null, null];
- }
+// Code inspired by Junior Developer Central - How to refresh/reload a page with JavaScript Youtube tutorial.
 
-  cards.forEach(card => card.addEventListener('click', flipCard));
+document.querySelector('#reload')
+
+document.querySelector('#reload').addEventListener('click', () =>
+{
+
+window.location.reload(true)
+
+})
+
+
+
+
+// code on how to match cards based on Marina Ferreira (freeCodeCamp.Org) Youtube video: "Memory Card Game-JavaScript Tutorial"
+
+// Match cards by data type, same card click (prevent card getting stuck if double clicked) and lock-board//
+
+const cards = document.querySelectorAll('.flip-card')
+
+
+
+
+let hasFlippedCard = false
+
+let lockBoard = false
+
+let firstCard, secondCard
+
+
+
+
+function flipCard() {
+
+// NOTE run the clock if the user flips a card
+
+clock()
+
+
+
+
+if (!gameOn) return
+
+if (lockBoard) return
+
+if (this === firstCard) return
+
+
+
+
+this.classList.add('flip')
+
+
+
+
+if (!hasFlippedCard) {
+
+hasFlippedCard = true
+
+firstCard = this
+
+return
+
+}
+
+
+
+
+secondCard = this
+
+
+
+
+checkForMatch()
+
+}
+
+
+
+
+// NOTE: isMatch is a boolean value, not int; it can't be used to determine in the game has ended.
+
+
+
+
+function checkForMatch() {
+
+let isMatch = firstCard.dataset.framework === secondCard.dataset.framework
+
+
+
+
+// NOTE: removed ternary if statement to add additional logic
+
+if (isMatch) {
+
+// increase the number of total matches made in the game
+
+disableCards()
+
+noOfMatches++
+
+} else {
+
+unflipCards()
+
+}
+
+
+
+
+if (noOfMatches === 8) {
+
+gameOn = false
+
+// alert(
+
+// 'game over! You took ' + currentTime + ' seconds to complete the game!'
+
+// )
+
+showmodalDisplayHandler(noOfMatches)
+
+}
+
+
+
+
+console.log(noOfMatches, currentTime)
+
+}
+
+
+
+
+function disableCards() {
+
+firstCard.removeEventListener('click', flipCard)
+
+secondCard.removeEventListener('click', flipCard)
+
+
+
+
+resetBoard()
+
+}
+
+
+
+
+function unflipCards() {
+
+lockBoard = true
+
+
+
+
+setTimeout(() => {
+
+firstCard.classList.remove('flip')
+
+secondCard.classList.remove('flip')
+
+
+
+
+resetBoard()
+
+}, 600)
+
+}
+
+
+
+
+function resetBoard() {
+
+;[hasFlippedCard, lockBoard] = [false, false]
+
+;[firstCard, secondCard] = [null, null]
+
+}
+
+
+
+
+cards.forEach((card) => card.addEventListener('click',
+flipCard))
+
+
+
 
 // Shuffle Cards CODE FROM Youtube Mar//
 
-(function shuffle() {
-    cards.forEach(card => {
-      let ramdomPos = Math.floor(Math.random() * 12);
-      card.style.order = ramdomPos;
-    });
-  })();
+;(function shuffle() {
+
+cards.forEach((card) => {
+
+let ramdomPos = Math.floor(Math.random()
+* 12)
+
+card.style.order = ramdomPos
+
+})
+
+})()
+
+
+
 
 //Timer code inspired by stackoverflow
 
 
-var myTimer;
+
+
+// NOTE changed "var" to "let" here
+
+let myTimer
+
+
+
+
 function clock() {
-     myTimer = setInterval(myClock, 1000);
-     var c = 60;
 
-     function myClock() {
-       document.getElementById("timer").innerHTML = --c;
-       if (c == 0) {
-         clearInterval(myTimer);
-         alert("Oh no, times up! You can continue this round untimed or click Reset to start again");
-       }
-     }
-   }
+if (myTimer) {
 
+return
 
-
-
-
-  //Congratulations Pop up message when board is completed -  NOT WORKING
-    //modal
-let modal = document.getElementById("popup1")
-function congratulations(){
-    if (matchedCard.length == 16){
-    modal.classList.add("show");
-    closeModal();
-    };
 }
 
-function winGame(){
-  if (isMatch === 16){
-     alert("Congratulations! You Won!");
-  }
- }
+if (!gameOn) {
+
+return
+
+}
+
+
+
+
+myTimer = setInterval(myClock, 1000)
+
+var c = 60
+
+
+
+
+function myClock() {
+
+document.getElementById('timer').innerHTML = --c
+
+currentTime = c
+
+if (c === 0) {
+
+gameOn = false
+
+clearInterval(myTimer)
+
+showmodalDisplayHandler(noOfMatches)
+
+} else if (!gameOn) {
+
+clearInterval(myTimer)
+
+return
+
+}
+
+}
+
+}
+
+
+
+
+//Congratulations Pop up message when board is completed - NOT WORKING
+
+//modal
+
+// let modal = document.getElementById('popup1')
+
+// function congratulations() {
+
+// if (matchedCard.length == 16) {
+
+// modal.classList.add('show')
+
+// closeModal()
+
+// }
+
+// }
+
+
+
+
+// NOTE: isMatch is a boolean value; it won't equal 16
+
+// NOTE: isMatch was declared with the "let" keyword inside of a function, which only allows it to be used inside of that function
+
+// function winGame() {
+
+// if (isMatch === 16) {
+
+// alert('Congratulations! You Won!')
+
+// }
+
+// }
+
 
 
 
 //WEBSCHOOL Rules of Game
+
 function on() {
-  document.getElementById("overlay").style.display = "block";
+
+document.getElementById('overlay').style.display = 'block'
+
 }
+
+
+
 
 function off() {
-  document.getElementById("overlay").style.display = "none";
+
+document.getElementById('overlay').style.display = 'none'
+
 }
 
 
 
 
+// =========================================================================
+
+// Add modals to screen
+
+// =========================================================================
 
 
 
-   
- 
+
+// declare the button so that we can listen for the user to click on it
+
+// const modalButton = document.querySelector('.modalDisplayButton')
+
+
+
+
+// when the user clicks on the button, the showModalHandler function will run
+
+// button.addEventListener('click', showmodalDisplayHandler)
+
+
+
+
+//=======================================================
+
+// Function that will put the modal onscreen
+
+//=======================================================
+
+
+
+
+function showmodalDisplayHandler(correctCards) {
+
+if (modalDisplay) {
+
+return
+
+}
+
+
+
+
+// if all didn't go well
+
+//if()
+
+
+
+
+// say that the modal will be a <div> element
+
+modalDisplay = document.createElement('div')
+
+modalDisplay.className = 'modalDisplay'
+
+
+
+
+// populate the modal with text
+
+const modalDisplayText = document.createElement('p')
+
+
+
+
+// if all went well
+
+if (correctCards === 8) {
+
+modalDisplayText.textContent =
+
+'Congratulations! You matched all cards correctly in ' +
+
+(60 - currentTime) +
+
+' seconds!'
+
+} else {
+
+modalDisplayText.textContent =
+
+'You ran out of time, but you matched ' + correctCards + ' pairs of cards'
+
+}
+
+
+
+
+// add the text to the modalDisplay
+
+modalDisplay.append(modalDisplayText)
+
+
+
+
+// create the <button> element inside of the modalDisplay
+
+const modalDisplayButton = document.createElement('button')
+
+
+
+
+// populate and style the button
+
+modalDisplayButton.textContent = 'Confirm'
+
+modalDisplayButton.className = 'modal-btn'
+
+
+
+
+// when the button is clicked, we close the modalDisplay
+
+modalDisplayButton.addEventListener('click', closemodalDisplayHandler)
+
+
+
+
+// attach the button to the modalDisplay
+
+modalDisplay.append(modalDisplayButton)
+
+
+
+
+// attach the modalDisplay to the body of the index.html page
+
+document.body.append(modalDisplay)
+
+
+
+
+// define and style the grey backgroundDisplay <div> that will sit behind the modalDisplay when its called to the screen
+
+backgroundDisplay = document.createElement('div')
+
+backgroundDisplay.className = 'backgroundDisplay'
+
+
+
+
+// if the backgroundDisplay is clicked, the modalDisplay will close
+
+backgroundDisplay.addEventListener('click', closemodalDisplayHandler)
+
+
+
+
+// attach the backgroundDisplay to the screen
+
+document.body.append(backgroundDisplay)
+
+}
+
+
+
+
+//=======================================================
+
+// Function that removes the modalDisplay from the screen
+
+//=======================================================
+
+function closemodalDisplayHandler() {
+
+// remove both from the screen
+
+modalDisplay.remove()
+
+backgroundDisplay.remove()
+
+// for modal to display again on screen 
+
+modalDisplay = null
+
+backgroundDisplay = null
+
+}
+
+
